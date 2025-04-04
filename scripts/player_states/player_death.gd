34428391964dfd85b364b4daf4a3d0055e62e034
Imo <immanuel.wagner@tutanota.de>
2025-04-04 16:24:01 +0200

    Update player_death.gd

--- scripts/player_states/player_death.gd
+++ scripts/player_states/player_death.gd
@@ -1,14 +1,11 @@
 extends PlayerBaseState
 # the death state of the player
 
-# Called when the state is entered.
 func enter(_owner):
 	pass
 
-# Called when the state is exited.
 func exit(_owner):
 	pass
 
-# Called every physics frame while this state is active.
 func physics_process(_owner, _delta):
 	pass
