8f4e10ff7f15185ed2b85dcce35e0ee93e5422b1
Imo <immanuel.wagner@tutanota.de>
2025-04-04 16:21:23 +0200

    added death script (empty) and lives variable
    Player Script doesn't seem to want to recognise the death state script when loading it

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,7 @@ func _ready():
 	states["STATE_RUN"] = preload("res://scripts/player_states/player_run.gd").new()
 	states["STATE_JUMP"] = preload("res://scripts/player_states/player_jump.gd").new()
 	states["STATE_ROLL"] = preload("res://scripts/player_states/player_roll.gd").new()
+	states["STATE_DEATH"] = preload("res://scripts/player_states/player_death.gd").new()
 	
 	# initial state
 	change_state("STATE_IDLE")
@@ -23,6 +24,9 @@ func _on_animation_finished() -> void:
 
 const SPEED = 100.0 # decides the speed of the player
 const JUMP_VELOCITY = -300.0 # decides the jump-speed of the player
+
+var lives = 3
+
 var was_on_floor = true  # tracks if the player was on the floor
 var is_jumping = false   # tracks if the player is in the jumping animation
 var direction = 0 # tracks the input direction
@@ -41,29 +45,33 @@ func change_state(new_state_name: String) -> void:
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
 	# Get the input direction: -1, 0, 1
 	direction = Input.get_axis("move_left", "move_right")
 	
-	# universal state changes
-	if not is_rolling:
-		if is_on_floor():
-			if direction != 0 and current_state != states["STATE_RUN"]:
-				change_state("STATE_RUN")
-			elif Input.is_action_just_pressed("jump") and current_state != states["STATE_JUMP"]:
+	# universal state changes; manages movement & death
+	if lives > 0:
+		if not is_rolling:
+			if is_on_floor():
+				if direction != 0 and current_state != states["STATE_RUN"]:
+					change_state("STATE_RUN")
+				elif Input.is_action_just_pressed("jump") and current_state != states["STATE_JUMP"]:
+					change_state("STATE_JUMP")
+				elif direction == 0 and current_state != states["STATE_IDLE"]:
+					change_state("STATE_IDLE")
+				elif Input.is_action_just_pressed("roll") and direction != 0:
+					change_state("STATE_ROLL")
+				else:
+					pass
+			elif current_state != states["STATE_JUMP"]:
 				change_state("STATE_JUMP")
-			elif direction == 0 and current_state != states["STATE_IDLE"]:
-				change_state("STATE_IDLE")
-			elif Input.is_action_just_pressed("roll") and direction != 0:
-				change_state("STATE_ROLL")
-			else:
-				pass
-		elif current_state != states["STATE_JUMP"]:
-			change_state("STATE_JUMP")
+		else:
+			pass
 	else:
-		pass
+		change_state("STATE_DEATH")
 	
 	# calling the physics_process of the current state (to execute it)
 	if current_state and current_state.has_method("physics_process"): 
