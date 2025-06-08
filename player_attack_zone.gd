d6f7f448e076351b42350be7c8abf726f2978ba0
Imo <immanuel.wagner@tutanota.de>
2025-06-08 11:10:49 +0200

    player_attack_zone reversion

--- player_attack_zone.gd
+++ player_attack_zone.gd
@@ -1,8 +1,6 @@
 extends Area2D
 
-func _on_body_entered(body: Node2D) -> void: # if the player enters the death zone, it should subtract one life from the player
-	if self.is_processing():
-		print("attack_zone enabled")
-		body.lose_lives(1)
-		print("PlayerAttackZone: player has hit enemy")
+func _on_body_entered(body: Node2D) -> void:
+	body.lose_lives(1)
+	print("PlayerAttackZone: player has hit enemy")
 	
